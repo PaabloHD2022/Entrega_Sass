733eb1d9be8e9a3348cb223187cff2a45e756284
PaabloHD2022 <pablodemartini@gmail.com>
2022-08-31 23:30:53 -0300

    Ultimos retoques al fetch

--- js/GestionarTiendaOnline.js
+++ js/GestionarTiendaOnline.js
@@ -1,33 +1,33 @@
-
 class TiendaOnline {
-
-    iniciar(productos){
-
-        fetch (url)
-        .then (respuesta => respuesta.json())
-        .then (resultado =>{
-            
-            console.log(resultado)
-
-            for (let producto of resultado){
-                let productoNuevo = new producto(productos.id, productos.Nombre, productos.PoduccionMinima, productos.Descripcion, productos.Img,)
-                TiendaOnline.push(productoNuevo)
-            }
-
-            mostrarProductos()            
-        })
+    iniciar() {
+        fetch(url)
+            .then((respuesta) => respuesta.json())
+            .then((resultado) => {
+                console.log(resultado);
+                const html = document.getElementById("productos");
+                const fragSect = document.createDocumentFragment();
+                resultado.forEach((item) => {
+                    const container = document.createElement("div");
+                    container.innerHTML = `<div>
+                                                <h5>${item.Nombre}</h5>
+                                                <p>${item.Descripcion}</p>
+                                                <p>${item.ProduccionMinima}</p>                       
+                                                <a href="./Contacto.html" class="btn btn-dark"></a>
+                                            </div>`;
+                    fragSect.appendChild(container);
+                });
+                html.appendChild(fragSect);
+            });
     }
 
-    cargarProductos(productos){
-
-        const divProductos = document.querySelector('#productos');
-        divProductos.innerHTML = '';
+    cargarProductos(productos) {
+        const divProductos = document.querySelector("#productos");
+        divProductos.innerHTML = "";
 
         productos.forEach((producto) => {
-
-            let prod = document.createElement ('div');
-            prod.classList.add('row', 'row-cols-1', 'row-cols-md-3 g-4');
-            prod.setAttribute('id', 'row_'+producto.id);
+            let prod = document.createElement("div");
+            prod.classList.add("row", "row-cols-1", "row-cols-md-3 g-4");
+            prod.setAttribute("id", "row_" + producto.id);
 
             prod.innerHTML += `  <div>
                                     <img src="./Img/Tienda_Online/${producto.img}" alt="">
@@ -37,10 +37,9 @@ class TiendaOnline {
                                         <p>${producto.PoduccionMinima}</p>                       
                                         <a href="./Contacto.html" class="btn btn-dark"></a>
                                     </div>
-                                </div>`
+                                </div>`;
 
             divProductos.appendChild(prod);
-
         });
     }
-};
+}
