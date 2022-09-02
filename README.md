ff0c9d14fb2a8d8f83ca7b2bfd365c2a3c8aeec4
PaabloHD2022 <pablodemartini@gmail.com>
2022-09-01 22:32:35 -0300

    Cambios en CSS y JS Tienda

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Trabajo-Practico-Pablo-Demartini
 
 Todavia me falta darle forma a los cards donde se ven los productos de la tienda. 
-El boton negro que se ve en la tienda tiene que decir "CONSULTA"
-Por alguna razon, no me esta reconociendo bien la ruta de las imagenes.
+El boton negro que se ve en la tienda tiene que decir "CONSULTA" - RESUELTO.
+Por alguna razon, no me esta reconociendo bien la ruta de las imagenes. - RESUELTO.
--- js/GestionarTiendaOnline.js
+++ js/GestionarTiendaOnline.js
@@ -11,8 +11,8 @@ class TiendaOnline {
                     container.innerHTML = ` <div class="row row-cols-1 row-cols-md-3 g-4">
                                                 <div class="col">
                                                     <div class="card h-100">
-                                                        <div class="card-body">
-                                                            <img src="${item.Img}" class="card-img-top">
+                                                        <img src="${item.Img}" class="card-img-top">
+                                                            <div class="card-body">                                                            
                                                             <h3 class="card-title">${item.Nombre}</h3>
                                                             <p class="card-text">${item.Descripcion}</p>
                                                             <h5 class="titulo2">${"Producción mínima" + " " + item.ProduccionMinima}</h5>                       
@@ -39,11 +39,11 @@ class TiendaOnline {
             prod.innerHTML += ` <div class="row row-cols-1 row-cols-md-3 g-4">
                                     <div class="col">
                                         <div class="card h-100">
-                                            <div class="card-body">
-                                                <img src="${item.Img}" class="card-img-top">
+                                            <img src="${item.Img}" class="card-img-top">
+                                                <div class="card-body">                                                            
                                                 <h3 class="card-title">${item.Nombre}</h3>
                                                 <p class="card-text">${item.Descripcion}</p>
-                                                <h5>${"Producción mínima" + " " + item.ProduccionMinima}</h5>                       
+                                                <h5 class="titulo2">${"Producción mínima" + " " + item.ProduccionMinima}</h5>                       
                                                 <a href="./Contacto.html" class="btn btn-dark">Consulta</a>
                                             </div>
                                         </div>
